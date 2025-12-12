2441b99d052bf37c093694d2ce15886fe70576dc
santiago202020 <143847324+santiago202020@users.noreply.github.com>
2025-12-11 22:53:48 -0500

    sin migraciones

--- bodegaISF/settings.py
+++ bodegaISF/settings.py
@@ -117,7 +117,15 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+# Desactivar sistema de migraciones
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+    
+    def __getitem__(self, item):
+        return None
 
+MIGRATION_MODULES = DisableMigrations()
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
