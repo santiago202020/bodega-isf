57a007d0009ee5df29ceb4d0f192aea1c0ad729a
santiago202020 <143847324+santiago202020@users.noreply.github.com>
2025-12-11 23:00:00 -0500

    cambios de settings

---
+++ bodegaISF/middleware.py
@@ -0,0 +1,9 @@
+class HealthCheckMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        if request.path == '/health/':
+            from django.http import HttpResponse
+            return HttpResponse("OK")
+        return self.get_response(request)
