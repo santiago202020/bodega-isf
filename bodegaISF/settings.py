042d42065683040b17aaf41adc0ea595f39741a5
santiago202020 <143847324+santiago202020@users.noreply.github.com>
2025-12-11 20:23:14 -0500

    configuracion de database

--- bodegaISF/settings.py
+++ bodegaISF/settings.py
@@ -95,15 +95,15 @@ WSGI_APPLICATION = 'bodegaISF.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bodega isf',
-        'USER': 'postgres',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
